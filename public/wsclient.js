0479698c78c30652e07619a5fe1985b159e0d05b
tsunamyh <fathpour.hojat@gmail.com>
2023-02-07 12:39:42 +0330

    commit

--- public/wsclient.js
+++ public/wsclient.js
@@ -1,4 +1,4 @@
-const HOST = location.href.replace(/^http/, 'ws');  //'ws://localhost:8080/echo'
+const HOST = location.href.replace(/^https/, 'ws');  //'ws://localhost:8080/echo'
 
 const ws = new WebSocket(HOST);
 
